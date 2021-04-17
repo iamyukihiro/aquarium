221009eb9d24922ad298c193ae5e738f308de987
Yukihiro Arisawa <goreboothero@gmail.com>
2021-04-17 17:14:18 +0900

    Aquariumの単体テストを追加

--- tests/AquariumTest.php
+++ tests/AquariumTest.php
@@ -4,20 +4,33 @@ declare(strict_types=1);
 
 namespace Goreboothero\Aquarium;
 
+use Goreboothero\Aquarium\Domain\Model\Fish\FishInterface;
+use Goreboothero\Aquarium\Domain\Model\Tank\Tank;
 use PHPUnit\Framework\TestCase;
 
 class AquariumTest extends TestCase
 {
-    protected Aquarium $aquarium;
-
-    protected function setUp(): void
-    {
-        $this->aquarium = new Aquarium();
-    }
-
     public function test(): void
     {
-        $actual = $this->aquarium;
+        $this->markTestSkipped('アクアリウムを楽しむという処理の仕様が決定していないのでスキップする');
+
+        $tankP = $this->prophesize(Tank::class);
+        $tank = $tankP->reveal();
+
+        $actual = $this->getSUT($tank, $fishers = []);
+
+        $actual->enjoy();
+
         $this->assertInstanceOf(Aquarium::class, $actual);
+    }
+
+    /**
+     * @param Tank $tank
+     * @param FishInterface[] $fishers
+     * @return Aquarium
+     */
+    public function getSUT(Tank $tank, array $fishers) : Aquarium
+    {
+        return new Aquarium($tank, $fishers);
     }
 }
