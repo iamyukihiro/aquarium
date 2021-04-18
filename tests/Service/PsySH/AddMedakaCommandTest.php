2f1bf098373b3c90ca0ca010e6a519e2ef690bc7
Yukihiro Arisawa <goreboothero@gmail.com>
2021-04-18 14:22:05 +0900

    add AddMedakaCommand

---
+++ tests/Service/PsySH/AddMedakaCommandTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Goreboothero\Aquarium\Service\PsySH;
+
+use Goreboothero\Aquarium\Infrastructure\LoadTank;
+use Goreboothero\Aquarium\Infrastructure\SaveTank;
+use PHPUnit\Framework\TestCase;
+
+class AddMedakaCommandTest extends TestCase
+{
+    public function test(): void
+    {
+        $path = dirname(__FILE__).'/../../.memory/tank_test.memory';
+
+        $saveTank = new SaveTank($path);
+        $loadTank = new LoadTank($path);
+
+        $this->getSUT($loadTank, $saveTank);
+    }
+
+    public function getSUT(LoadTank $loadTank, SaveTank $saveTank) : AddMedakaCommand
+    {
+        return new AddMedakaCommand($loadTank, $saveTank);
+    }
+}
