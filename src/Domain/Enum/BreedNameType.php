f9a945def57952ad497fafcc293bf4e95e3517ec
Yukihiro Arisawa <goreboothero@gmail.com>
2024-05-01 10:56:56 +0900

    add: Breed

--- src/Domain/Enum/BreedNameType.php
+++ src/Domain/Enum/BreedNameType.php
@@ -7,9 +7,12 @@ namespace Iamyukihiro\Aquarium\Domain\Enum;
 class BreedNameType
 {
     public const HI_MEDAKA = '緋目高';
-    public const MIYUKI = 'みゆき';
+    public const MIYUKI = '幹之';
     public const YOUKIHI = '楊貴妃';
     public const OROCHI = 'オロチ';
+    public const KURO = 'クロ';
+    public const SHIRO = 'シロ';
+    public const YOZAKURA = '夜桜';
 
     /** @return string[] */
     public static function getBreedNameForMedaka(): array
@@ -19,6 +22,9 @@ class BreedNameType
             self::MIYUKI,
             self::YOUKIHI,
             self::OROCHI,
+            self::KURO,
+            self::SHIRO,
+            self::YOZAKURA,
         ];
     }
 }
